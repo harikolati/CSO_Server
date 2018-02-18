c164e159e3d6aef5ed61cdd088f99ea4fb4c7c6e
harikolati <kolati.hari@gmail.com>
2018-02-18 21:05:50 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,33 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+var appRoutes = require('./routes/app');
+var dealRoutes = require('./routes/deals');
+var app = express();
+var fileupload=require('express-fileupload');
+app.use(fileupload());
+mongoose.connect('mongodb://localhost/M&A');
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.setHeader('Access-Control-Allow-Methods', 'POST, GET, PATCH, DELETE, OPTIONS');
+    next();
+});
+app.use('/deals', dealRoutes);
+app.use('/', appRoutes);
+
+
+
+
+module.exports = app;
---
+++ models/deal.js
@@ -0,0 +1,34 @@
+var mongoose = require('mongoose');
+var AutoIncrement = require('mongoose-sequence')(mongoose);
+var Buffer = require('buffer/').Buffer
+var dealSchema = mongoose.Schema({
+companyLogo : String,
+targetCompanyName : String,
+dealCode : String,
+transactionType : String,
+purchasePrice : String, 
+publicrPriviate :String,
+dealType : String,
+marketSegment :[String],
+sponsoringGroupName : String,
+groupDescription : String,
+pressAnnounceSource : String,
+companyDescription : String,
+announceYear : String,
+closeQuarter : String,
+targetMarketSegment : [String],
+followedUsers : [String],
+monetizationModel : String,
+transactionStage : String,
+empCountonAnnounce : Number,
+employeesOnBoarded : Number,
+createdBy : String,
+updatedBy : String,
+createdAt : { type: Date},
+updatedAt :{ type: Date},
+},
+{ collection : 'Deals' }
+).plugin(AutoIncrement, {inc_field: 'dealNumber'});;
+var Deal = mongoose.model('Deal', dealSchema);
+ 
+module.exports = Deal;
